0bc8062a2ce03cafc15b8910ebbb62c3bd5d03f3
Saad <saadali2969@gmail.com>
2025-11-14 10:53:25 +0500

    Clean upload

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AdvDS_FILE D:/Adv DS Visualizer/AdvDS/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/AdvDS.exe)
-set(__QT_DEPLOY_TARGET_AdvDS_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AdvDS_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
